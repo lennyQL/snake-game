66ddb0ab72c88a304fb2e68503574a4bd4cd284b
Lenny <skyzs.9097@gmail.com>
2020-06-20 20:59:30 +0900

    Updates

---
+++ precache-manifest.96e7211a1be5e4f08d750d57a5285683.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23708586642abe0f92d5d6ccb0c3c7c",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "9d85e3a9f4ac4a615457",
+    "url": "/snake-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4cf4280835c2df607f0f",
+    "url": "/snake-game/static/js/2.267c16d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game/static/js/2.267c16d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d85e3a9f4ac4a615457",
+    "url": "/snake-game/static/js/main.690a96b3.chunk.js"
+  },
+  {
+    "revision": "0866649720b7e6a02873",
+    "url": "/snake-game/static/js/runtime-main.e42eead7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/snake-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
